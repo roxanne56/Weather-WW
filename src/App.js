ed41ce5a2afb166c70bab655eb61fe14b72f0740
Roxana Moraru <47501710+roxanne56@users.noreply.github.com>
2023-04-15 19:25:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Glass from "./Glass";
+
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+
+  const searchLocation = (event) => { 
+   if (event.key === "Enter") {	
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=17cb251ef11f70d37fe5738624f7c587`;
+    axios.get(url).then((response) => {
+      setData(response.data);
+  })
+    setLocation ('')
+  }
+}
+
+//in div search trebuie adaugat removeClass "Wellcome" message on click in enter location
+
+  return (
+    <div className="app">
+    <div className="search">
+      <input 
+      value={location}
+      onChange={event => setLocation(event.target.value)}
+      onKeyPress={searchLocation}
+      placeholder="Enter Location"
+      type="text"/>
+
+    </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> :null}
+          </div>
+          
+          <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        <Glass />
+        {data.name !== undefined &&
+        <div className="bottom">
+          <div className="feels">
+          {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+          <p>Feels Like</p>
+          </div>
+
+          <div className="humidity">
+             {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p >Humidity</p>
+          </div>
+
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+           <p>Wind Speed</p>
+          </div>
+        </div>
+        }
+
+      </div>
+      
+    </div>
+  );
+}
+
+  
+
+
+export default App;
---
+++ src/Glass.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Glass=() => {
+    return (
+        <div className="glass">
+            <form className="form">
+        <h1> Wellcome ! </h1>
+        <h4>Let the weather inspire you to be your best self today! </h4>
+            </form>
+        </div>
+    );
+}   
+
+export default Glass;
+
+
